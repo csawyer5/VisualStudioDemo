e346f6819789d7ff89454e21aa12605991fb9243
Chris Sawyer <chris5sawyer@hotmail.com>
2019-12-10 13:40:21 -0600

    THis is stupid

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace VisualStudioDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        
         }
     }
 }
